76a184c5643eb198398b609a9021427965bad9f2
Carla <ctacha29@hotmail.com>
2025-11-25 01:13:00 -0600

    videos

--- app_crud/index.php
+++ app_crud/index.php
@@ -25,7 +25,7 @@ $resultado = null;
 <body>
     <div class="container mt-5">
         <h1 class="mb-4 text-center">Gestión de Empleados</h1>
-        <h2>Dirección de Recursos Humanos</h2>
+        <h2 class="mb-4 text-center">Dirección de Recursos Humanos</h2>
         
         <a href="agregar.php" class="btn btn-primary mb-3">
             <i class="fas fa-plus-circle"></i> Agregar Nuevo Usuario
@@ -66,8 +66,46 @@ $resultado = null;
                 ?>
             </tbody>
         </table>
-    </div>
+
+</table>
+    </div>
     
+    <div class="container mt-5 mb-5">
+        <hr>
+        <h2 class="mb-4 text-center">Videos Relacionados</h2>
+        <div class="row">
+            
+            <div class="col-md-4 mb-4">
+                <div class="ratio ratio-16x9">
+                    
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/2M5NziF3Jc0?si=9Ua7JX3m-4g8Xa4H" title="YouTube video player" 
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                    </iframe>             
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="ratio ratio-16x9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Z5UZAKjJ6Io?si=debjAefhj1lScAeP" title="YouTube video player" 
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+                <div class="ratio ratio-16x9">
+                 <iframe width="560" height="315" src="https://www.youtube.com/embed/GHBhBkJjWD4?si=0CXwDQN8vvYyPDHN" title="YouTube video player"
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  
+
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
